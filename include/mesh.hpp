963dcedbbe5aa7861498c42e664b06e38731e6d6
Physicist137 <physicist093@yahoo.com.br>
2018-08-16 16:21:24 -0300

    Fixing few mistakes. Declaring isInside() function

--- include/mesh.hpp
+++ include/mesh.hpp
@@ -21,7 +21,7 @@ class Mesh {
 	std::vector<std::array<int, 2>> _edges;
 	std::vector<std::array<int, 3>> _triangles;
 
-public
+public:
 	// The Mesh::Edge class.
 	class Edge{
 		std::array<int, 2> _ids;
@@ -50,6 +50,8 @@ public
 			Vertex<FloatingPoint, Value>& v2,
 			Vertex<FloatingPoint, Value>& v3
 		) 	: _vertex1(v1), _vertex2(v2), _vertex3(v3) {}
+
+		bool isInside(const Vertex<FloatingPoint, Value>& vertex) const;
 	};
 
 public:
@@ -64,10 +66,14 @@ public:
 	// Functions for constructing triangles.
 	int addTriangleBlindlyFromVertices(int v1, int v2, int v3);
 
-	// Add a vertex, near a triangle, creating another triangle.
-	int integrateVertex(int triangle, const Vertex<FloatingPoint, Value>& vertex);
+	// Add a vertex, inside a triangle, creating four triangles.
+	int addVertexInsideTriangle(int triangle, const Vertex<FloatingPoint, Value>& vertex);
 };
 
+template <typename FloatingPoint, typename Value>
+bool Mesh<FloatingPoint, Value>::Triangle::isInside(const Vertex<FloatingPoint, Value>& vertex) const {
+
+}
 
 template <typename FloatingPoint, typename Value>
 typename Mesh<FloatingPoint, Value>::Triangle Mesh<FloatingPoint, Value>::triangle(int id) {
@@ -86,7 +92,7 @@ int Mesh<FloatingPoint, Value>::addVertex(const Vertex<FloatingPoint, Value>& ve
 
 template <typename FloatingPoint, typename Value>
 int Mesh<FloatingPoint, Value>::addEdgeBlindly(int v1, int v2) {
-	_edge.push_back({v1, v2});
+	_edges.push_back({v1, v2});
 	return _edges.size() - 1;
 }
 
@@ -97,6 +103,6 @@ int Mesh<FloatingPoint, Value>::addTriangleBlindlyFromVertices(int v1, int v2, i
 }
 
 template <typename FloatingPoint, typename Value>
-int integrateVertex(int triangle, const Vertex<FloatingPoint, Value>::triangle(int id) {
+int Mesh<FloatingPoint, Value>::addVertexInsideTriangle(int triangle, const Vertex<FloatingPoint, Value>& vertex) {
 	// TODO.
 }
