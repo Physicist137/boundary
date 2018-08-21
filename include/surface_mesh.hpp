14e901929dc22d540ad80b464ebd9aa769840d82
Physicist137 <physicist093@yahoo.com.br>
2018-08-21 11:54:34 -0300

    Fixing mistakes on Surface Mesh Class

--- include/surface_mesh.hpp
+++ include/surface_mesh.hpp
@@ -4,40 +4,40 @@
 
 template <typename FloatingPoint, typename Value>
 class Point {
-	Matrix<FloatingPoint, 2, 1> _point;
+	Eigen::Matrix<FloatingPoint, 2, 1> _point;
 	Value _value;
 
 public:
-	Point(const Matrix<FloatingPoint, 2, 1> point, const Value& value)
+	Point(const Eigen::Matrix<FloatingPoint, 2, 1>& point, const Value& value)
 	: _point(point), _value(value) {}
 
-	inline const Matrix<FloatingPoint, 2, 1>& point() const {return _point;}
+	inline const Eigen::Matrix<FloatingPoint, 2, 1>& point() const {return _point;}
 	inline const Value& value() const {return _value;}
 };
 
 template <typename FloatingPoint, typename Value>
 Point<FloatingPoint, Value> operator+(const Point<FloatingPoint, Value>& a, const Point<FloatingPoint, Value>& b) {
-	return Point(a.point() + b.point(), a.value() + b.value());
+	return Point<FloatingPoint, Value>(a.point() + b.point(), a.value() + b.value());
 }
 
 template <typename FloatingPoint, typename Value>
 Point<FloatingPoint, Value> operator-(const Point<FloatingPoint, Value>& a, const Point<FloatingPoint, Value>& b) {
-	return Point(a.point() - b.point(), a.value() - b.value());
+	return Point<FloatingPoint, Value>(a.point() - b.point(), a.value() - b.value());
 }
 
 template <typename FloatingPoint, typename Value>
 Point<FloatingPoint, Value> operator*(const Point<FloatingPoint, Value>& a, const FloatingPoint& num) {
-	return Point(a.point() * num, a.value() * num);
+	return Point<FloatingPoint, Value>(a.point() * num, a.value() * num);
 }
 
 template <typename FloatingPoint, typename Value>
 Point<FloatingPoint, Value> operator* (const FloatingPoint& num, const Point<FloatingPoint, Value>& a) {
-	return Point(a.point() * num, a.value() * num);
+	return Point<FloatingPoint, Value>(a.point() * num, a.value() * num);
 }
 
 template <typename FloatingPoint, typename Value>
 Point<FloatingPoint, Value> operator/(const Point<FloatingPoint, Value>& a, const FloatingPoint& num) {
-	return Point(a.point() / num, a.value() / num);
+	return Point<FloatingPoint, Value>(a.point() / num, a.value() / num);
 }
 
 // ----------------------------------------------------------
@@ -48,7 +48,7 @@ class SurfaceMesh {
 public:
 	// Accessor functions.
 	inline const Point<FloatingPoint, Value>& point(int id) const {return _points[id];}
-	inline const std::vector<Point<FloatingPoint, Value>>::size_type size() const {return _points.size();}
+	inline const typename std::vector<Point<FloatingPoint, Value>>::size_type size() const {return _points.size();}
 
 	// Tangent values.
 	Point<FloatingPoint, Value> forwardTangent(int id) const;
@@ -61,21 +61,21 @@ public:
 
 template <typename FloatingPoint, typename Value>
 Point<FloatingPoint, Value> SurfaceMesh<FloatingPoint, Value>::forwardTangent(int id) const {
-	return _point[i+1] - _point[i];
+	return _points[id+1] - _points[id];
 }
 
 template <typename FloatingPoint, typename Value>
 Point<FloatingPoint, Value> SurfaceMesh<FloatingPoint, Value>::backwardTangent(int id) const {
-	return _point[i] - _point[i-1];
+	return _points[id] - _points[id-1];
 }
 
 template <typename FloatingPoint, typename Value>
 Point<FloatingPoint, Value> SurfaceMesh<FloatingPoint, Value>::centralTangent(int id) const {
-	return (_point[i+1] - point[i-1]) / 2.0;
+	return (_points[id+1] - _points[id-1]) / 2.0;
 }
 
 template <typename FloatingPoint, typename Value>
 Eigen::Matrix<FloatingPoint, 2, 1>& SurfaceMesh<FloatingPoint, Value>::normalVector
-(const Eigen::Matrix<FloatingPoint, 2, 1>& tangent) const;
-	return Matrix<FloatingPoint, 2, 1>{tangent(1,0), -tangent(0,0)};
-};
+(const Eigen::Matrix<FloatingPoint, 2, 1>& tangent) const {
+	return Eigen::Matrix<FloatingPoint, 2, 1>{-tangent(1,0), -tangent(0,0)};
+}
