7fc9f144a14e0100a91d5e198d6954ff61ae4391
Physicist137 <physicist093@yahoo.com.br>
2018-08-21 10:31:33 -0300

    Adding a Surface Mesh Point class

--- include/surface_mesh.hpp
+++ include/surface_mesh.hpp
@@ -2,7 +2,45 @@
 #include<vector>
 #include<Eigen/Dense>
 
-// The boundary.
+template <typename FloatingPoint, typename Value>
+class Point {
+	Matrix<FloatingPoint, 2, 1> _point;
+	Value _value;
+
+public:
+	Point(const Matrix<FloatingPoint, 2, 1> point, const Value& value)
+	: _point(point), _value(value) {}
+
+	inline const Matrix<FloatingPoint, 2, 1>& point() const {return _point;}
+	inline const Value& value() const {return _value;}
+};
+
+template <typename FloatingPoint, typename Value>
+Point<FloatingPoint, Value> operator+(const Point<FloatingPoint, Value>& a, const Point<FloatingPoint, Value>& b) {
+	return Point(a.point() + b.point(), a.value() + b.value());
+}
+
+template <typename FloatingPoint, typename Value>
+Point<FloatingPoint, Value> operator-(const Point<FloatingPoint, Value>& a, const Point<FloatingPoint, Value>& b) {
+	return Point(a.point() - b.point(), a.value() - b.value());
+}
+
+template <typename FloatingPoint, typename Value>
+Point<FloatingPoint, Value> operator*(const Point<FloatingPoint, Value>& a, const FloatingPoint& num) {
+	return Point(a.point() * num, a.value() * num);
+}
+
+template <typename FloatingPoint, typename Value>
+Point<FloatingPoint, Value> operator* (const FloatingPoint& num, const Point<FloatingPoint, Value>& a) {
+	return Point(a.point() * num, a.value() * num);
+}
+
+template <typename FloatingPoint, typename Value>
+Point<FloatingPoint, Value> operator/(const Point<FloatingPoint, Value>& a, const FloatingPoint& num) {
+	return Point(a.point() / num, a.value() / num);
+}
+
+// ----------------------------------------------------------
 template <typename FloatingPoint, typename Value>
 class SurfaceMesh {
 	std::vector< Matrix<FloatingPoint, 2, 1> > _points;
@@ -10,8 +48,9 @@ class SurfaceMesh {
 
 public:
 	// Accessor functions.
-	const Matrix<FloatingPoint, 2, 1>& point(int id) const;
-	const Value& value(int id) const;
+	inline const Matrix<FloatingPoint, 2, 1>& point(int id) const {return _points[id];}
+	inline const Value& value(int id) const {return _value[id];}
+	inline const std::vector<Value>::size_type size() const {return _value.size();}
 
 	// Tangent values.
 	Matrix<FloatingPoint, 2, 1> forwardTangent(int id) const;
@@ -21,6 +60,7 @@ public:
 	// Normal Values.
 	Matrix<FloatingPoint, 2, 1> forwardNormal(int id) const;
 	Matrix<FloatingPoint, 2, 1> backwardNormal(int id) const;
-	Matrix<FloatingPoint, 2, 1> middleNormal(int id) const;
-	
+	Matrix<FloatingPoint, 2, 1> middleNormal(int id) const;	
 };
+
+
